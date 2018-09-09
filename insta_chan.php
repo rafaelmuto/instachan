1d19c8d2647d63f32e305c74a47ed23e9a841ab5
rafaelmuto <r.nagahama@gmail.com>
2018-09-09 06:26:03 -0300

    styling remodel

--- insta_chan.php
+++ insta_chan.php
@@ -32,20 +32,76 @@
         margin: 0;
         padding: 0;
       }
+
+      body{
+        padding: 10px;
+      }
+
+      fieldset{
+        width: 600px;
+        margin: 0 auto;
+        padding: 10px;
+      }
+
+      input{
+        border: 0;
+        border-bottom: 1px solid black;
+        margin-bottom: .5em;
+      }
+
+      textarea{
+        resize: none;
+        width: 100%;
+      }
+
+      .line{
+        width: 100%;
+      }
+
+      .btn {
+        padding: 1rem 2rem;
+        border: 1px solid black;
+        background: transparent;
+        font-size: 0.9em;
+        border-radius: 2px;
+        margin: 0  auto;
+      }
+
+      .btn:hover {
+        cursor: pointer;
+        background: black;
+        color: white;
+      }
+
+      .img_thumb{
+        /* float: left; */
+        width: 100px;
+        height: auto;
+      }
+
+      .msg{
+
+      }
+
+
     </style>
+
     <title>InstaChan</title>
   </head>
   <body>
-    <h1>InstaChan v.0</h1>
-    a one file solution for image BBS.
-    <hr>
-    <form action="#" method="post" enctype="multipart/form-data">
-      <label for="user">User:</label><input type="text" name="user" value=""><br>
-      <label for="passw">Password:</label><input type="Password" name="passw" value=""><br>
-      <label for="file">Image:</label><input type="file" name="img" value=""><br>
-      <label for="msg">Message:</label><br><textarea name="msg" rows="8" cols="80"></textarea><br>
-      <input type="submit" name="Submit">
-    </form>
+    <fieldset>
+      <legend>version 0</legend>
+      <h1>InstaChan</h1>
+      a one file solution for image BBS.
+      <hr>
+      <form action="#" method="post" enctype="multipart/form-data">
+        <label for="user">User:</label><input class="line" type="text" name="user" value=""><br>
+        <label for="passw">Password:</label><input class="line" type="Password" name="passw" value=""><br>
+        <label for="file">Image:</label><input class="line" type="file" name="img" value=""><br>
+        <label for="msg">Message:</label><br><textarea name="msg" rows="8" cols="80"></textarea><br>
+        <button class="btn" type="submit" name="button">submit</button>
+      </form>
+    </fieldset>
     <?php
       if($_POST!=[]){
         $array["instadb"][] = add_data();
@@ -57,12 +113,12 @@
         $move_ok = move_uploaded_file($file, "img/".$file_name);
       }
      ?>
-    <hr><pre>
+    <br><hr>
     <?php
     $array["instadb"] = array_reverse($array["instadb"],TRUE);
     foreach ($array["instadb"] as $id => $item) {
-      echo "#" . $id . " @ " . $item["time"] . "<br>" . $item["user"] . ":<br>" . $item["msg"];
-      if($item["img_path"] != FALSE) echo '<img src="' . $item["img_path"] . '">';
+      echo '#' . $id . ' @ ' . $item["time"] . '<br>' . $item["user"] . ':<br><p class="msg">' . $item["msg"] . '</p><br>';
+      if($item["img_path"] != FALSE) echo '<a href="' . $item["img_path"] . '"><img class="img_thumb" src="' . $item["img_path"] . '"></a>';
       echo "<hr>";
     } ?>
 
