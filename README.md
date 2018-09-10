b858cc2b94402abc9809f8beddb787742fc7c402
rafaelmuto <r.nagahama@gmail.com>
2018-09-09 22:40:47 -0300

    v0 complete

--- README.md
+++ README.md
@@ -6,6 +6,13 @@ This is a study for a one file image board BBS using PHP and JSON.
 This is a work in progress...
 
 ---
+## To Do...
+
+.implement securty against code injection
+.auto delete board after n number of post
+.cotinous paper background...
+
+---
 ## History
 
-v0 -- Basic text and image function, JSON "database"
+v0 -- Basic text and image function, JSON "database". Post can be posted and deleted.
--- insta_chan.php
+++ insta_chan.php
@@ -1,3 +1,6 @@
+<!-- InstaChan v0-->
+<!-- a one file solution for image BBS. -->
+<!-- by: rafaelmuto -->
 <!DOCTYPE html>
 <?php
   if(file_exists("instadb.json")){
@@ -10,35 +13,32 @@
   if(!is_dir("img/")){
     mkdir("img/");
   }
-
-  function add_data(){
-    $temp_array = [];
-    $temp_array["time"] = date("YmdHis");
-    $temp_array["user"] = $_POST["user"];
-    $temp_array["passw"] = password_hash($_POST["passw"], PASSWORD_DEFAULT);
-    $temp_array["msg"] = $_POST["msg"];
-    if($_FILES["img"]["error"]==UPLOAD_ERR_OK) $temp_array["img_path"] = "img/".$_FILES["img"]["name"];
-    else $temp_array["img_path"]=FALSE;
-    return $temp_array;
-  }
-
  ?>
+
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
     <style media="screen">
+      :root{
+        --max_w: 950px;
+        --thumb_w: 100px;
+        --color_post_odd: #ecf0f1;
+        --color_post_even: #bdc3c7;
+      }
       *{
         font-family: monospace;
         margin: 0;
         padding: 0;
+        box-sizing: border-box;
       }
 
       body{
         padding: 10px;
       }
 
-      fieldset{
-        width: 600px;
+      .form_post{
+        width: var(--max_w);
+        border: 1px solid;
         margin: 0 auto;
         padding: 10px;
       }
@@ -73,49 +73,125 @@
         color: white;
       }
 
+      .board{
+        max-width: var(--max_w);
+        margin: 0 auto;
+      }
+
+      .post{
+        border-bottom: 1px solid;
+        padding: 5px;
+        padding-top: 0;
+        display: grid;
+        grid-template-columns: var(--thumb_w) auto;
+        grid-template-rows: auto auto auto;
+        grid-template-areas: "post_header post_header" "img post_msg";
+      }
+
+      .post:nth-child(odd){
+        background-color: var(--color_post_odd);
+      }
+
+      .post:nth-child(even){
+        background-color: var(--color_post_even);
+      }
+
+      .post_header{
+        grid-column: span 2;
+        grid-area: post_header;
+      }
+
       .img_thumb{
-        /* float: left; */
-        width: 100px;
+        width: var(--thumb_w);
         height: auto;
+        grid-area: img;
+      }
+
+      .post_msg{
+        margin-left: 5px;
+        grid-area: post_msg;
+      }
+
+      .del_post{
+        max-width: var(--max_w);
+        padding: 10px;
+        margin: 0 auto;
+        border: 1px solid;
+        border-top: 0;
       }
 
     </style>
 
     <title>InstaChan</title>
   </head>
+
   <body>
-    <fieldset>
-      <legend>version 0</legend>
-      <h1>InstaChan</h1>
+
+    <!-- POST FORM -->
+    <div class="form_post">
+      <h1>InstaChan v0</h1>
       a one file solution for image BBS.
       <hr>
       <form action="#" method="post" enctype="multipart/form-data">
         <label for="user">User:</label><input class="line" type="text" name="user" value=""><br>
-        <label for="passw">Password:</label><input class="line" type="Password" name="passw" value=""><br>
+        <label for="passw">Delete Code:</label><input class="line" type="Password" name="passw" value=""><br>
         <label for="file">Image:</label><input class="line" type="file" name="img" value=""><br>
         <label for="msg">Message:</label><br><textarea name="msg" rows="8" cols="80"></textarea><br>
         <button class="btn" type="submit" name="button">submit</button>
       </form>
-    </fieldset>
+    </div>
+
+    <!-- Instadb -->
     <?php
-      if($_POST!=[]){
-        $array["instadb"][] = add_data();
+      if($_GET!=[]){
+        foreach ($array["instadb"] as $id => $item) {
+          if($_GET["del_id"] == $item["time"] && password_verify($_GET["del_code"],$item["passw"])){
+            unset($array["instadb"]["$id"]);
+          }
+        }
         file_put_contents("instadb.json",json_encode($array));
       }
-      if($_POST!=[] && $_FILES["img"]["error"]==UPLOAD_ERR_OK){
-        $file_name = $_FILES["img"]["name"];
-        $file = $_FILES["img"]["tmp_name"];
-        $move_ok = move_uploaded_file($file, "img/".$file_name);
+
+      if($_POST!=[] && $_POST["user"]!=[] && $_POST["msg"]!=[] && $_POST["passw"]!=[]){
+        $post["time"] = date("YmdHis");
+        $post["user"] = $_POST["user"];
+        $post["passw"] = password_hash($_POST["passw"], PASSWORD_DEFAULT);
+        $post["msg"] = $_POST["msg"];
+        if($_FILES["img"]["error"]==UPLOAD_ERR_OK){
+          $file_ext = explode(".", strtolower($_FILES["img"]["name"]));
+          $file_name = $post["time"] .".". $file_ext[1];
+          $file = $_FILES["img"]["tmp_name"];
+          $move_ok = move_uploaded_file($file, "img/".$file_name);
+          $post["img_path"] = "img/".$file_name;
+        }
+        else $post["img_path"]=FALSE;
+        $array["instadb"][] = $post;
+        file_put_contents("instadb.json",json_encode($array));
       }
+
      ?>
-    <br><hr>
-    <?php
-    $array["instadb"] = array_reverse($array["instadb"],TRUE);
-    foreach ($array["instadb"] as $id => $item) {
-      echo '#' . $id . ' @ ' . $item["time"] . ' >> <strong>' . $item["user"] . ':</strong><br><p class="msg">' . $item["msg"] . '</p><br>';
-      if($item["img_path"] != FALSE) echo '<a href="' . $item["img_path"] . '"><img class="img_thumb" src="' . $item["img_path"] . '"></a>';
-      echo "<hr>";
-    } ?>
+
+    <!-- BOARD -->
+    <div class="board">
+      <?php
+      $array["instadb"] = array_reverse($array["instadb"],TRUE);
+      foreach ($array["instadb"] as $id => $item) {
+        echo '<div class="post">';
+        echo '<p class="post_header">#' . $id . ' id: ' . $item["time"] . ' >> <strong>' . $item["user"] . ':</strong><br></p>';
+        if($item["img_path"] != FALSE) echo '<a href="' . $item["img_path"] . '"><img class="img_thumb" src="' . $item["img_path"] . '"></a>';
+        echo '<p class="post_msg">' . $item["msg"] . '</p>';
+        echo "</div>";
+      } ?>
+    </div>
+
+    <!-- DELETE FORM -->
+    <div class="del_post">
+      <form action="#" method="get">
+        id: <input type="text" name="del_id">
+        del_code: <input type="password" name="del_code">
+        <button type="submit" name="submit">delete</button>
+      </form>
+    </div>
 
   </body>
 </html>
